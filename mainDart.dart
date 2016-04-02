534e8ed1ab5064d74591dc1358dc23554a0352b7
Sohail Mohammed <sohailm498@gmail.com>
2016-04-01 22:26:28 -0400

    clear list button for ToDoList

--- mainDart.dart
+++ mainDart.dart
@@ -2,12 +2,15 @@ import 'dart:html';
 
 InputElement toDoInput;
 UListElement outputList;
+ButtonElement clearAll;
 
 void main() {
   querySelector('#chgText').text = 'Lets Program in Dart!';
   toDoInput = querySelector('#addItem');
   outputList = querySelector('#displayList');
   toDoInput.onChange.listen(toDoItemsList);
+  clearAll = querySelector('#clearList');
+  clearAll.onClick.listen((e) => outputList.children.clear());
 }
 
 void toDoItemsList(Event e){
@@ -16,4 +19,5 @@ void toDoItemsList(Event e){
   newToDo.onClick.listen((e) => newToDo.remove()); 
   toDoInput.value ='';
   outputList.children.add(newToDo);
+
 }
