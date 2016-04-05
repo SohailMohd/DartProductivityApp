00bda177365f8db2aaa4108aaaa3d884ee7dd4b3
Sohail Mohammed <sohailm498@gmail.com>
2016-04-04 23:21:28 -0400

    minor changes to toDOList and added check notes button

--- mainDart.dart
+++ mainDart.dart
@@ -1,8 +1,11 @@
 import 'dart:html';
+import 'dart:convert';
+//import 'dart:io';
 
 InputElement toDoInput;
 UListElement outputList;
 ButtonElement clearAll;
+var checkNotes;
 
 void main() {
   querySelector('#chgText').text = 'Lets Program in Dart!';
@@ -11,13 +14,43 @@ void main() {
   toDoInput.onChange.listen(toDoItemsList);
   clearAll = querySelector('#clearList');
   clearAll.onClick.listen((e) => outputList.children.clear());
+  var readContents = new File('Notes.txt').readAsStringnSync();
+
+  querySelector('#displayNotes').text = readContents;
+  //checkNotes = querySelector('#displayNotes');
+
 }
 
 void toDoItemsList(Event e){
   var newToDo = new LIElement();
   newToDo.text = toDoInput.value;
-  newToDo.onClick.listen((e) => newToDo.remove()); 
+  newToDo.onClick.listen((e) => newToDo.remove());
   toDoInput.value ='';
   outputList.children.add(newToDo);
 
 }
+
+
+
+/*
+void makeRequest(Event e) {
+  var path = 'https://www.dartlang.org/samples-files/portmanteaux.json';
+  var httpRequest = new HttpRequest();
+  httpRequest
+    ..open('GET', path)
+    ..onLoadEnd.listen((e) => requestComplete(httpRequest))
+    ..send('');
+}
+
+requestComplete(HttpRequest request) {
+  if (request.status == 200) {
+    List<String> portmanteaux = JSON.decode(request.responseText);
+    for (int i = 0; i < portmanteaux.length; i++) {
+      wordList.children.add(new LIElement()..text = portmanteaux[i]);
+    }
+  } else {
+    wordList.children.add(new LIElement()
+      ..text = 'Request failed, status=${request.status}');
+  }
+}
+*/
