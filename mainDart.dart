7f8b824d92cec7037a49fcf1643c156626ada097
Sohail Mohammed <sohailm498@gmail.com>
2016-04-16 23:18:33 -0400

    added functionality to calculator

--- mainDart.dart
+++ mainDart.dart
@@ -1,17 +1,22 @@
 import 'dart:html';
 import 'dart:convert';
+//import 'dart:io';
 //import 'package:js/js.dart' as js;
+/*
 import 'package:intl/intl.dart';
 import 'package:slideshow/slideshow.dart';
 import 'package:slideshow/transition_.dart';
 import 'package:polymer/polymer.dart';
 import 'package:paper_elements/paper_input.dart';
-
+*/
 
 
 InputElement toDoInput;
+InputElement takeInput;
 UListElement outputList;
 ButtonElement clearAll;
+OutputElement showNum;
+
 var checkNotes;
 
 void main() {
@@ -23,18 +28,30 @@ void main() {
   clearAll = querySelector('#clearList');
   clearAll.onClick.listen((e) => outputList.children.clear());
 
-  final Slideshow test = new Slideshow(
+  /*final Slideshow test = new Slideshow(
         document.body.querySelector("#notesImgsBox"),
-        new Transition());
+        new Transition()); */
 
   for (Element e in querySelectorAll('a')) {
     e.onClick.listen((e) => handleClick(e.target));
   }
+
+    takeInput = querySelector('#num1');
+    takeInput.onClick.listen(assignNumValues);
+    //takeInput.onClick.listen(querySelector('#calcScreen'));
+
   //var readContents = new File('Notes.txt').readAsStringnSync();
   //querySelector('#displayNotes').text = readContents;
   //checkNotes = querySelector('#displayNotes');
 
 
+}
+
+void assignNumValues(Event e){
+  var getValue = new LIElement();
+  getValue.text = takeInput.value;
+  //toDoInput.value ='';
+  //outputList.children.add(newToDo);
 }
 
 void toDoItemsList(Event e){
